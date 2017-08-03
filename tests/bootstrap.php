a855b305bf1eacacc710e1fbec3fc8b035d72aad
jeremykendall <jeremy@jeremykendall.net>
2017-08-03 10:28:02 -0500

    Add phpunit as dev dependency

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,22 +6,4 @@ error_reporting(-1);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-set_include_path(implode(PATH_SEPARATOR, array(
-        dirname(__FILE__) . '/../library',
-        get_include_path()
-        )
-    )
-);
-
-function autoload($class)
-{
-    include str_replace('_', DIRECTORY_SEPARATOR, $class) . '.php';
-}
-
-function namespaceAutoload($class) 
-{
-    include str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
-}
-
-spl_autoload_register('namespaceAutoload');
-spl_autoload_register('autoload');
+require_once __DIR__ . '/../vendor/autoload.php';
